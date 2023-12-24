ebd45c5a6a8c2eb958c2f361e2c956d464675975
tianfy <2573427870@qq.com>
2023-12-24 11:07:50 +0800

    test

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world(a: int):
+    return str(a)
 
+@app.route('/process_list', methods=['POST'])
+def process_list():
+    data = request.get_json() # 固定写法
+
+    task_id = data["task_id"]
+    method_name = data["method_name"]
+    print(f"task_id: {task_id}, method_name: {method_name}")
+    return f"task_id: {task_id}, method_name: {method_name}"
+
+
+
+@app.route('/config/methodList')
+def a():
+    return json.dumps(['a','b'])
 
 if __name__ == '__main__':
     app.run()
